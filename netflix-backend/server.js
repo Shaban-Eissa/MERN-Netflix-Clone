a15658cf8167ecb191d01c49ed0262c2bdbfd023
Shaban-Eissa <shabaneissa767@gmail.com>
2023-12-17 01:12:17 +0200

    MERN DEPLOY ON VERCEL

--- netflix-backend/server.js
+++ netflix-backend/server.js
@@ -2,14 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const path = require("path");
 const userRoutes = require("./routes/UserRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
 
 mongoose
   .connect(process.env.MONGODB_URL, {
@@ -19,9 +17,6 @@ mongoose
   .then(() => console.log("MongoDB connected!"));
 
 app.use("/api/user", userRoutes);
-app.get("/google44809cf0d25f012f.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "google44809cf0d25f012f.html"));
-});
 
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
